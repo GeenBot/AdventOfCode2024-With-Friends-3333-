44f1dc1e08d1bf4e15d61a75fad55c38af06c612
Rosie <acquitefx@gmail.com>
2024-12-01 18:27:48 +0000

    Rose Day 1

---
+++ Solutions/Rose/2024/01/solution.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+def lists(x):
+    ls = []
+    rs = []
+
+    with open(x, 'r') as file:
+        for line in file:
+            l, r = map(int, line.split())
+            ls.append(l)
+            rs.append(r)
+
+    return (ls, rs)
+
+name = "input.txt"
+ls, rs = lists(name)
+
+def s1():
+    lss = sorted(ls)
+    rss = sorted(rs)
+    dist = sum(abs(l - r) for l, r in zip(lss, rss))
+    return dist
+
+def s2():
+    rsc = Counter(rs)
+    score = sum(x * rsc[x] for x in ls)
+    return score
+
+print(s1())
+print(s2())
