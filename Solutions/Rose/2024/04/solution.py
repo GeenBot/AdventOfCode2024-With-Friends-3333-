f277cd87205fdc69f31abbef19d6d6d159912211
Rosie <acquitefx@gmail.com>
2024-12-05 05:54:03 +0000

    Day 5

--- Solutions/Rose/2024/04/solution.py
+++ Solutions/Rose/2024/04/solution.py
@@ -1,42 +1,48 @@
-def s1(grid):
-    rows, cols = len(grid), len(grid[0])
-    word = "XMAS"
-    count = 0
-    dirs = [(0, 1),(0, -1),(1, 0),(-1, 0),(1, 1),(-1, -1),(1, -1),(-1, 1)]
+# def s1(grid):
+#     rows, cols = len(grid), len(grid[0])
+#     word = "XMAS"
+#     count = 0
+#     dirs = [(0, 1),(0, -1),(1, 0),(-1, 0),(1, 1),(-1, -1),(1, -1),(-1, 1)]
 
-    def check(x, y, dx, dy):
-        for k in range(len(word)):
-            nx, ny = x + k * dx, y + k * dy
-            if not (0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == word[k]):
-                return False
-        return True
+#     def check(x, y, dx, dy):
+#         for k in range(len(word)):
+#             nx, ny = x + k * dx, y + k * dy
+#             if not (0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == word[k]):
+#                 return False
+#         return True
 
-    for r in range(rows):
-        for c in range(cols):
-            for dx, dy in dirs:
-                if check(r, c, dx, dy):
-                    count += 1
+#     for r in range(rows):
+#         for c in range(cols):
+#             for dx, dy in dirs:
+#                 if check(r, c, dx, dy):
+#                     count += 1
 
-    return count
+#     return count
 
-def s2(grid):
-    rows, cols = len(grid), len(grid[0])
-    count = 0
-    pat = [["M", "A", "S"], ["S", "A", "M"]]
+# def s2(grid):
+#     rows, cols = len(grid), len(grid[0])
+#     count = 0
+#     pat = [["M", "A", "S"], ["S", "A", "M"]]
 
-    def check(x, y):
-        p1 = [grid[x][y], grid[x+1][y+1], grid[x+2][y+2]]
-        p2 = [grid[x][y+2], grid[x+1][y+1], grid[x+2][y]]
-        return p1 in pat and p2 in pat
+#     def check(x, y):
+#         p1 = [grid[x][y], grid[x+1][y+1], grid[x+2][y+2]]
+#         p2 = [grid[x][y+2], grid[x+1][y+1], grid[x+2][y]]
+#         return p1 in pat and p2 in pat
 
-    for i in range(rows - 2):
-        for j in range(cols - 2):
-            if check(i, j):
-                count += 1
+#     for i in range(rows - 2):
+#         for j in range(cols - 2):
+#             if check(i, j):
+#                 count += 1
 
-    return count
+#     return count
 
-with open('input.txt', 'r') as f:
-    grid = [list(l.strip()) for l in f.readlines()]
-    print(s1(grid))
-    print(s2(grid))
+# with open('input.txt', 'r') as f:
+#     grid = [list(l.strip()) for l in f.readlines()]
+#     print(s1(grid))
+#     print(s2(grid))
+
+s=open("input.txt").read()
+q=lambda j:f(j)+f(-j)
+n=m=0
+for i in range(len(s)):f=lambda j:s[i::j][:4]=="XMAS";n+=q(1)+q(w:=s.find("\n"))+q(w+1)+q(w+2);f=lambda j:s[i-j::j][:3]=="MAS";m+=q(w)*q(w+2)
+print(n,m)
---
+++ Solutions/Rose/2024/05/solution.py
@@ -0,0 +1,49 @@
+contents = open("input.txt").read()
+rd, ud = contents.split('\n\n')
+rs = [tuple(map(int, r.split('|'))) for r in rd.splitlines()]
+us = [list(map(int, u.split(','))) for u in ud.splitlines()]
+
+def is_ordered(u):
+    for x, y in rs:
+        if x in u and y in u:
+            if u.index(x) > u.index(y):
+                return False
+    return True
+
+def reorder(u):
+    while True:
+        s = False
+        for i in range(len(u) - 1):
+            x, y = u[i], u[i + 1]
+            for rx, ry in rs:
+                if x == rx and y == ry:
+                    u[i], u[i + 1] = u[i + 1], u[i]
+                    s = True
+                    break
+        if not s:
+            break
+    return u
+
+def s1():
+    res = []
+
+    for u in us:
+        if is_ordered(u):
+            mid = len(u) // 2
+            res.append(u[mid])
+
+    return sum(res)
+
+def s2():
+    res = []
+
+    for u in us:
+        if not is_ordered(u):
+            u2 = reorder(u)
+            mid = len(u2) // 2
+            res.append(u2[mid])
+
+    return sum(res)
+
+print(s1())
+print(s2())
