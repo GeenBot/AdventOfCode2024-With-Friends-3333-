2a3329504675eb564ce5286491c2306bfb469ce3
Rosie <acquitefx@gmail.com>
2024-12-14 05:57:56 +0000

    Day 14 & partially working Day 12 in Elle

--- Solutions/Rose/2024/14/solution.py
+++ Solutions/Rose/2024/14/solution.py
@@ -1,12 +1,62 @@
 import sys
 
-def s1(c):
-    pass
+rows = 101
+cols = 103
 
-def s2(c):
-    pass
+def parse(c):
+    robots = []
+    for line in [line for line in c if line.strip() != ""]:
+        p, v = line.split()
+        px, py = map(int, p[2:].split(","))
+        vx, vy = map(int, v[2:].split(","))
+        robots.append(((px, py), (vx, vy)))
+    return robots
+
+def quad(nx, ny, ans):
+    if nx == rows // 2 or ny == cols // 2:
+        return
+    if nx < rows // 2 and ny < cols // 2:
+        ans[0] += 1
+    elif nx > rows // 2 and ny < cols // 2:
+        ans[1] += 1
+    elif nx < rows // 2 and ny > cols // 2:
+        ans[2] += 1
+    else:
+        ans[3] += 1
+
+def s1(robots):
+    ans = [0, 0, 0, 0]
+    for (px, py), (vx, vy) in robots:
+        nx, ny = px + 100 * vx, py + 100 * vy
+        nx %= rows
+        ny %= cols
+        quad(nx, ny, ans)
+    return ans[0] * ans[1] * ans[2] * ans[3]
+
+def s2(robots):
+    s = 0
+    while True:
+        grid = [[0 for _ in range(rows)] for _ in range(cols)]
+        s += 1
+
+        inv = False
+        for (px, py), (vx, vy) in robots:
+            nx, ny = px + s * vx, py + s * vy
+            nx %= rows
+            ny %= cols
+
+            grid[ny][nx] += 1
+            if grid[ny][nx] > 1:
+                inv = True
+
+        if not inv:
+            # for row in grid:
+            #     print("".join(map(str, row)).replace("0", "."))
+            break
+    return s
 
 with open(sys.argv[1]) as f:
-    c = f.read()
-    print(s1(c))
-    print(s2(c))
+    c = f.readlines()
+    robots = parse(c)
+    print(s1(robots))
+    print(s2(robots))
