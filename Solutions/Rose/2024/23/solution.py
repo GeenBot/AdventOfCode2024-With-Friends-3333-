6fc90187af35226f179696b359eefe3baf823c72
Rosie <acquitefx@gmail.com>
2024-12-23 14:07:51 +0000

    Make Day 23 smaller

--- Solutions/Rose/2024/23/solution.py
+++ Solutions/Rose/2024/23/solution.py
@@ -11,20 +11,19 @@ def find_ts(graph):
     ts = []
     for t in nx.enumerate_all_cliques(graph):
         if len(t) == 3:
-            ts.append(tuple(sorted(t)))
+            ts.append(tuple(t))
     return ts
 
-def s1(cons):
-    graph = build(cons)
+def s1(graph):
     ts = find_ts(graph)
     return sum(1 for t in ts if any(node.startswith('t') for node in t))
 
-def s2(cons):
-    graph = build(cons)
+def s2(graph):
     clique = max(list(nx.find_cliques(graph)), key=len)
     return ','.join(sorted(clique))
 
 with open(sys.argv[1]) as f:
     cons = [line.strip().split('-') for line in f.read().strip().split('\n')]
-    print(s1(cons))
-    print(s2(cons))
+    graph = build(cons)
+    print(s1(graph))
+    print(s2(graph))
